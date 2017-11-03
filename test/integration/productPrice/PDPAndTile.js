1fce48cc4c902559f00882ee7ed1cea99ae6e66f
Zi Sardone <zsardone@salesforce.com>
2017-11-02 23:32:08 -0400

    RAP-7156-singlePid

--- test/integration/productPrice/PDPAndTile.js
+++ test/integration/productPrice/PDPAndTile.js
@@ -8,36 +8,36 @@ describe('check product price', function () {
     this.timeout(5000);
 
     it('Product price in PDP and Grid should be the same', function () {
-        var Pids = ['25688302', '25503585'];
-        Pids.forEach(function (pid) {
-            console.log('verifying the product ', pid);
-            verifyProductPrice(pid);
-        });
-
-        function verifyProductPrice(Pid) {
-            var PDP;
-            var myRequest = {
-                url: config.baseUrl + '/Test-Product?pid=' + Pid,
-                method: 'GET',
-                rejectUnauthorized: false,
-                resolveWithFullResponse: true
-            };
-            return request(myRequest)
-                .then(function (response) {
-                    var bodyAsJsonPdp = JSON.parse(response.body);
-                    assert.equal(response.statusCode, 200, 'Expected Test-Product statusCode to be 200.');
-                    myRequest.url = config.baseUrl + '/Test-Product?pid=' + Pid + '&pview=tile';
-                    PDP = bodyAsJsonPdp.product;
-                    console.log('PDP is ', PDP);
-                    return request(myRequest);
-                })
-                .then(function (response2) {
-                    var tile = JSON.parse(response2.body);
-                    assert.equal(response2.statusCode, 200, 'Expected Test-Product statusCode to be 200.');
-                    console.log('tile is ', tile.product);
-                });
+        var Pids = '25688302';
+        // Pids.forEach(function (pid) {
+        //     console.log('verifying the product ', pid);
+        //     verifyProductPrice(pid);
+        // });
+        //
+        var PDP;
+        var tile;
+        var myRequest = {
+            url: config.baseUrl + '/Test-Product?pid=' + Pids,
+            method: 'GET',
+            rejectUnauthorized: false,
+            resolveWithFullResponse: true
         };
-
+        return request(myRequest)
+            .then(function (response) {
+                var bodyAsJsonPdp = JSON.parse(response.body);
+                assert.equal(response.statusCode, 200, 'Expected Test-Product statusCode to be 200.');
+                myRequest.url = config.baseUrl + '/Test-Product?pid=' + Pids + '&pview=tile';
+                PDP = bodyAsJsonPdp.product.price;
+                console.log('PDP price is ', PDP);
+                return request(myRequest);
+            })
+            .then(function (response2) {
+                var bodyAsJsonTile = JSON.parse(response2.body);
+                assert.equal(response2.statusCode, 200, 'Expected Test-Product statusCode to be 200.');
+                tile = bodyAsJsonTile.product.price;
+                console.log('Tile price is ', tile);
+            })
+            .then(() => assert.deepEqual(PDP, tile));
     });
-})
+});
 
