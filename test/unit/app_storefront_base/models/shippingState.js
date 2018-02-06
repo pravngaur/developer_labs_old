b5229bd19614b3bc8380be9315af2efe9f43ef19
Syed Shehroz Hussain (Ozzy) <s.hussain@salesforce.com>
2018-02-06 14:08:32 -0500

    Enhancements, test and lint fixes

--- test/unit/app_storefront_base/models/shippingState.js
+++ test/unit/app_storefront_base/models/shippingState.js
@@ -23,30 +23,38 @@ var orderMock2 = {
         },
         shippingAddress: 'someAddressForDelivery'
     }]
+};
 
+var orderMock3 = {
+    shipping: orderMock2.shipping.concat(orderMock.shipping)
 };
 
 describe('shippingState', function () {
-    it('should receive an null shippingState', function () {
-        var result = new ShippingStateModel(null);
-        assert.deepEqual(result.shippingState, null);
-    });
-
     it('should return an address for pickup in store', function () {
         var result = new ShippingStateModel(orderMock);
-        assert.equal(result.shippingState[0].shipmentUUID, '123');
-        assert.equal(result.shippingState[0].methodID, '009');
-        assert.equal(result.shippingState[0].pickupAddress, 'someStoreForPickUp');
-        assert.isTrue(result.shippingState[0].pickupEnabled, true);
+        assert.equal(result.shippingState.shipments[0].shipmentUUID, '123');
+        assert.equal(result.shippingState.shipments[0].methodID, '009');
+        assert.equal(result.shippingState.shipments[0].pickupAddress, 'someStoreForPickUp');
+        assert.isTrue(result.shippingState.shipments[0].pickupEnabled, true);
     });
 
 
     it('should return an address for delivery', function () {
         var result = new ShippingStateModel(orderMock2);
-        assert.equal(result.shippingState[0].shipmentUUID, '345');
-        assert.equal(result.shippingState[0].methodID, '008');
-        assert.equal(result.shippingState[0].deliveryAddress, 'someAddressForDelivery');
-        assert.isFalse(result.shippingState[0].pickupEnabled, false);
+        assert.equal(result.shippingState.shipments[0].shipmentUUID, '345');
+        assert.equal(result.shippingState.shipments[0].methodID, '008');
+        assert.equal(result.shippingState.shipments[0].deliveryAddress, 'someAddressForDelivery');
+        assert.isFalse(result.shippingState.shipments[0].pickupEnabled, false);
+    });
+
+    it('should return multiship as true for more than one shipment', function () {
+        var result = new ShippingStateModel(orderMock3);
+        assert.equal(result.shippingState.multiship, true);
+    });
+
+    it('should return multiship as false for only one shipment', function () {
+        var result = new ShippingStateModel(orderMock);
+        assert.equal(result.shippingState.multiship, false);
     });
 });
 
