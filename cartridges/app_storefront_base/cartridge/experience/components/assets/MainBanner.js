9adff8124bbddf33891fccc9be4c4648864eab3f
Syed Shehroz Hussain (Ozzy) <s.hussain@salesforce.com>
2019-06-03 16:12:45 -0400

    Page designer main banner component (#483)

---
+++ cartridges/app_storefront_base/cartridge/experience/components/assets/MainBanner.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+
+/**
+ * Render logic for Main Banner.
+ * @param {dw.experience.PageScriptContext} context The page script context object.
+ * @returns {string} template to be displayed
+ */
+module.exports.render = function (context) {
+    var model = new HashMap();
+    var content = context.content;
+
+    var mobileImageTransformation = ImageTransformation.scale(content.image.metaData, 'mobile');
+    var tabletImageTransformation = ImageTransformation.scale(content.image.metaData, 'tablet');
+    var desktopImageTransformation = ImageTransformation.scale(content.image.metaData, 'desktop');
+
+    model.heading = content.heading;
+    model.image = {
+        src: {
+            mobile: ImageTransformation.url(content.image.file, mobileImageTransformation),
+            tablet: ImageTransformation.url(content.image.file, tabletImageTransformation),
+            desktop: ImageTransformation.url(content.image.file, desktopImageTransformation)
+        },
+        alt: content.image.file.getAlt(),
+        focalPointX: (content.image.focalPoint.x * 100) + '%',
+        focalPointY: (content.image.focalPoint.y * 100) + '%'
+    };
+    model.categoryLink = URLUtils.url('Search-Show', 'cgid', content.categoryLink.getID()).toString();
+    return new Template('experience/components/mainBanner').render(model).text;
+};
