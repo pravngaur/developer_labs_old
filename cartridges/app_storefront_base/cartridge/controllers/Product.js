b292618592983bbf1d55fd510b43b3b186271e65
Gurpreet Saini <gurpreet.saini@salesforce.com>
2019-07-19 09:35:51 -0600

    Promotions HTML rendered on server

--- cartridges/app_storefront_base/cartridge/controllers/Product.js
+++ cartridges/app_storefront_base/cartridge/controllers/Product.js
@@ -70,10 +70,10 @@ server.get('Variation', function (req, res, next) {
         attributeContext,
         attributeTemplate
     );
-    
+
     var promotionsContext = { product: { promotions: product.promotions } };
     var promotionsTemplate = 'product/components/promotions';
-    
+
     product.promotionsHtml = renderTemplateHelper.getRenderedHtml(
         promotionsContext,
         promotionsTemplate
