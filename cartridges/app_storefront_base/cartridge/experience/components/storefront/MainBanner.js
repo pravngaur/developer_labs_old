698d35fb9edf060743a0e95f1f241d456f52d0f5
anndiep-sfcc <41341914+anndiep-sfcc@users.noreply.github.com>
2019-07-30 12:11:17 -0400

    Update: Page Designer component - Update Shop Category Promotion Banner Configurations (W-6330388) (#577)

--- cartridges/app_storefront_base/cartridge/experience/components/storefront/MainBanner.js
+++ cartridges/app_storefront_base/cartridge/experience/components/storefront/MainBanner.js
@@ -14,21 +14,9 @@ module.exports.render = function (context) {
     var model = new HashMap();
     var content = context.content;
 
-    var mobileImageTransformation = ImageTransformation.scale(content.image.metaData, 'mobile');
-    var tabletImageTransformation = ImageTransformation.scale(content.image.metaData, 'tablet');
-    var desktopImageTransformation = ImageTransformation.scale(content.image.metaData, 'desktop');
-
     model.heading = content.heading;
-    model.image = {
-        src: {
-            mobile: ImageTransformation.url(content.image.file, mobileImageTransformation),
-            tablet: ImageTransformation.url(content.image.file, tabletImageTransformation),
-            desktop: ImageTransformation.url(content.image.file, desktopImageTransformation)
-        },
-        alt: content.image.file.getAlt(),
-        focalPointX: (content.image.focalPoint.x * 100) + '%',
-        focalPointY: (content.image.focalPoint.y * 100) + '%'
-    };
+    model.image = ImageTransformation.getScaledImage(content.image);
     model.categoryLink = URLUtils.url('Search-Show', 'cgid', content.categoryLink.getID()).toString();
+
     return new Template('experience/components/assets/mainBanner').render(model).text;
 };
