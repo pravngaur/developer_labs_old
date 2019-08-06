b2043f820a60e39b92bca0bdd5409b3a3769c901
Ann Diep <adiep@salesforce.com>
2019-08-05 23:35:06 -0400

    New: Page Designer component - Look Book Layout (W-6379119)

---
+++ cartridges/app_storefront_base/cartridge/client/default/js/mobileGridLookBook.js
@@ -0,0 +1,29 @@
+'use strict';
+
+$(document).ready(function () {
+    $('body').on('click', '.show-more-button', function (e) {
+        e.preventDefault();
+
+        var $set2Element = $(this).closest('.look-book-layout').find('.look-book-set2');
+        $set2Element.removeClass('hide-set');
+
+        var $showMoreElement = $(this).closest('.look-book-layout').find('.show-more');
+        $showMoreElement.addClass('d-none');
+
+        var $hideMoreElement = $(this).closest('.look-book-layout').find('.hide-more');
+        $hideMoreElement.removeClass('d-none');
+    });
+
+    $('body').on('click', '.hide-more-button', function (e) {
+        e.preventDefault();
+
+        var $set2Element = $(this).closest('.look-book-layout').find('.look-book-set2');
+        $set2Element.addClass('hide-set');
+
+        var $showMoreElement = $(this).closest('.look-book-layout').find('.show-more');
+        $showMoreElement.removeClass('d-none');
+
+        var $hideMoreElement = $(this).closest('.look-book-layout').find('.hide-more');
+        $hideMoreElement.addClass('d-none');
+    });
+});
