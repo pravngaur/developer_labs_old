ab7f5463bea6309ab91b1169e967f5703c9972b1
Praveen Gaur <pgaur@pgaur-ltm.internal.salesforce.com>
2020-02-09 06:43:56 +0530

    lab_2

--- cartridges/plugin_controller_override/cartridge/controllers/Home.js
+++ cartridges/plugin_controller_override/cartridge/controllers/Home.js
@@ -4,6 +4,7 @@ var UrlUtils = require('dw/web/URLUtils');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
 var cache = require('*/cartridge/scripts/middleware/cache');
+var Site = require('dw/system/Site');
 
 server.extend(module.superModule);
 
@@ -13,14 +14,18 @@ server.extend(module.superModule);
  * New middleware added to verify the country & request locale -- if not a pair, redirect to correct locale.
  */
 server.prepend('Show',consentTracking.consent, cache.applyDefaultCache, function(req, res, next) {
-    var countryCode = req.geolocation.countryCode;
-    var locale = req.locale;
-    if ((countryCode && countryCode === 'US') && (locale && locale.id !== 'en_US')){
-        localeHelper(req, res, 'en_US');
-    } else if ((countryCode && countryCode === 'GB') && (locale && locale.id !== 'en_GB')){
-        localeHelper(req, res, 'en_GB');
-    } else if ((countryCode && countryCode === 'IN') && (locale && locale.id !== 'en_IN')){
-        localeHelper(req, res, 'en_IN');
+    // Placing the redirection logic behind the enableLocaleRedirect toggle -- evaluation redirection if toggle is set to true.
+    var customPrefs = Site.getCurrent().getPreferences().getCustom();
+    if (('enableLocaleRedirect' in customPrefs) && customPrefs.enableLocaleRedirect) {
+        var countryCode = req.geolocation.countryCode;
+        var locale = req.locale;
+        if ((countryCode && countryCode === 'US') && (locale && locale.id !== 'en_US')) {
+            localeHelper(req, res, 'en_US');
+        } else if ((countryCode && countryCode === 'GB') && (locale && locale.id !== 'en_GB')) {
+            localeHelper(req, res, 'en_GB');
+        } else if ((countryCode && countryCode === 'IN') && (locale && locale.id !== 'en_IN')) {
+            localeHelper(req, res, 'en_IN');
+        }
     }
     next();
 }, pageMetaData.computedPageMetaData);
